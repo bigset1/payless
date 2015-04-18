99a51fadb585f9320ba445fee4dbf66163cef83e
Anton Nikulin <antonnik94@gmail.com>
2015-04-18 15:40:39 +0300

    Small bug

--- server/src/main/java/com/letionik/payless/server/service/impl/ConversionUtils.java
+++ server/src/main/java/com/letionik/payless/server/service/impl/ConversionUtils.java
@@ -5,7 +5,6 @@ import com.letionik.payless.server.persistance.model.ProductBO;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * @author Roman Kishchenko
