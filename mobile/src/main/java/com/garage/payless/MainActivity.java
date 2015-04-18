39ade6307fd8ae276779dcb4993f232c5b91c8c4
Parishkura Roman <parishkura.roma@gmail.com>
2015-04-18 13:24:41 +0300

    implemented fetching location, request adding item

--- mobile/src/main/java/com/garage/payless/MainActivity.java
+++ mobile/src/main/java/com/garage/payless/MainActivity.java
@@ -3,8 +3,13 @@ package com.garage.payless;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.garage.payless.entity.LocationEvent;
 import com.garage.payless.fragment.FragmentMain;
+import com.garage.payless.util.ReactiveLocationHelper;
+
+import de.greenrobot.event.EventBus;
 
 
 public class MainActivity extends FragmentActivity {
@@ -19,5 +24,23 @@ public class MainActivity extends FragmentActivity {
 
         fragmentMain = FragmentMain.newInstance();
         FragmentHelper.add(fragmentManager, fragmentMain, FRAME_CONTAINER);
+
+        new ReactiveLocationHelper().requestUpdateLocation(this, 2);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        EventBus.getDefault().unregister(this);
+    }
+
+    public void onEvent(LocationEvent event) {
+        Toast.makeText(this, event.getLatLng().toString(), Toast.LENGTH_SHORT).show();
     }
 }
--- mobile/src/main/java/com/garage/payless/api/ResponseCallback.java
+++ mobile/src/main/java/com/garage/payless/api/ResponseCallback.java
@@ -1,7 +1,5 @@
 package com.garage.payless.api;
 
-import com.garage.payless.entity.Response;
-
 /**
  * Created by Paryshkura Roman on 18.04.2015.
  */
