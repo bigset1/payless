505509fed0db61974e975fb12e819136aea12cc4
Anton Nikulin <antonnik94@gmail.com>
2015-04-18 15:33:58 +0300

    Updated stub

--- server/src/main/java/com/letionik/payless/server/service/impl/BasketServiceImpl.java
+++ server/src/main/java/com/letionik/payless/server/service/impl/BasketServiceImpl.java
@@ -2,11 +2,14 @@ package com.letionik.payless.server.service.impl;
 
 import com.letionik.payless.model.Store;
 import com.letionik.payless.model.transport.BasketSearchResult;
+import com.letionik.payless.server.error.DataAccessExceptionMapper;
 import com.letionik.payless.server.service.BasketService;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Anton Nikulin
@@ -19,7 +22,10 @@ public class BasketServiceImpl implements BasketService {
 			double longitude) {
 		//TODO: replace stub with real implementation
 		Store testStore = new Store("test_brand", 55.66, 44.55, "10:00 - 23:00");
-		BasketSearchResult basketSearchResult = new BasketSearchResult(testStore, 34.5, null);
+		Map<String, Double> productPrices = new HashMap<String, Double>();
+		productPrices.put("123123", 3.45);
+		productPrices.put("234234", 7.34);
+		BasketSearchResult basketSearchResult = new BasketSearchResult(testStore, 34.5, productPrices);
 		return Arrays.asList(basketSearchResult);
 	}
 }
